9e35d9c4119719612fa33e333bf3467e23488677
mohanstar <98344550+mohanstar@users.noreply.github.com>
2022-01-28 23:38:12 +0530

    Update hellow.java

--- hellow.java
+++ hellow.java
@@ -1,1 +1,6 @@
-hello java from git
+public class Hello {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
